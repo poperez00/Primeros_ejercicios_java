8bc88cfa4aa7b4f0f21cef4348b43f4f69ddc544
Inma Olias <ioliasa@gmail.com>
2021-01-06 20:33:28 +0100

    Ejercicios para empezar con Java

---
+++ src/method/Ejercicio.java
@@ -0,0 +1,70 @@
+package method;
+
+
+public class Ejercicio {
+	
+	public static final Double PI = 3.14;
+	
+
+	public static void main(String[] args) {
+				
+		// Variables
+		
+		// Inicio
+		
+				
+				
+	}
+	
+	public static int numeroSolucionesEcuacionSegundoGrado(int a, int b, int c) {
+		
+	}
+	
+	public static Double solucionSumaEcuacionSegundoGrado(int a, int b, int c) {
+		
+	}
+	
+	
+	
+	public static Double solucionRestaEcuacionSegundoGrado(int a, int b, int c) {
+		
+	}	
+	
+	public static Double areaCirculo(Double r) {
+		
+	}
+	
+	public static Double longitudCirculo(Double r) {
+		
+	}
+	
+	public static boolean esMultiplo(int a, int b) {
+		
+	}
+	
+	public static int horaMayor(int hora1, int min1, int seg1, int hora2, int min2, int seg2) {
+		
+		
+	}
+	
+	public static int segundosEntre(int hora1, int min1, int seg1, int hora2, int min2, int seg2) {
+				
+		
+	}
+	
+	public static int maximoComunDivisor(int a, int b) {
+		
+	}
+	
+	public static int minimoComunMultiplo(int a, int b) {
+		
+	}
+	
+	public static String binario(int num) {
+		
+	}
+	
+	public static int decimal(String num) {
+		
+	}
+}
